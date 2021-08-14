b351d8de8d86bb28f0a949d9b91ca577206df8f6
Luke Peterson <luke@infoplatter.com>
2021-08-13 22:40:53 -0400

    Cleaning up and rounding out "Future Work" documentation section

--- src/lib.rs
+++ src/lib.rs
@@ -117,7 +117,8 @@
 //! DB contents are encoded using the [bincode] crate.  Currently the database contains 4 Column Families.
 //! 
 //! 1. The "rec_data" CF uses a little-endian-encoded [RecordID] as its key, and stores a varint-encoded `Vec` of
-//!     integers, which represent key_group indices, each of which contains at least one key associated with the record.
+//!     integers, which represent key_group indices, each of which can be combined with a `RecordID` to create a
+//!     `KeyGroupID`.  Each referenced key_group contains at least one key associated with the record.
 //!     The `rec_data` CF is the place to start when constructing the complete set of keys associated with a record.
 //! 
 //! 2. The "keys" CF uses a little-endian-encoded `KeyGroupID` as its key, and stores a varint-encoded `Vec` of
@@ -134,29 +135,115 @@
 //! 
 //! ## Future Work
 //! 
-//! 1. Optimization for crowded neighborhoods in the key metric-space.
+//! 1. Optimization for crowded neighborhoods in the key metric-space.  The current design optimizes for the case
+//!     where a single record has multiple keys with overlapping variants.  This is an efficient design in the
+//!     case where many proximate keys belong to the same record, as is the case when multiple spelling variations
+//!     for the same word are submitted to the Table.
 //! 
-//! 2. Multi-threading within a lookup. Rework internal plumbing so that variant entries can be fetched and processed
+//!     First, here is some background on how we got to this design.  In v0.2.0, multi-key
+//!     support was added because many use cases involved creating multiple records that were conceptually the same
+//!     record in order to represent key variations such as multiple valid spellings of the same thing. Becauase
+//!     these keys are often similar to each other and shared many variants in the database, the key_groups feature
+//!     was added to improve performance by reducing the number of keys in each variant entry.  Ideally
+//!     a given variant entry would only contain one reference to a record even if that record had many similar keys.
+//! 
+//!     Unfortunately this meant all the record's keys would be loaded together and that lead to unnecessary
+//!     invocations of the distance function to evaluate all of the keys for each record.  The solution in v0.2.0
+//!     was to segment a record's keys into key_groups, so similar keys would be together in the same key group
+//!     but other keys needn't be tested.  This solution works well for individual records with many keys that
+//!     cluster together.
+//! 
+//!     However, a large performance problem still exists when many keys from *Different* records cluster together.
+//!     To address this, I think that a different database layout is needed, and here is my thinking:
+//! 
+//!     - Create a separate CF to store full keys is needed, and each key entry would contain the [RecordID]s
+//!         assiciated with that key. There are two possible implementations:
+//! 
+//!         - A more compact implementation for longer keys would be to have a "KeyID" to reference each key entry.
+//!             Then the entry would contain the key itself, and the [RecordID]s associated with the key in the same
+//!             entry or in another CF that is also indexed by `KeyID`.  In this implementation, the variant CF is
+//!             still used to find the KeyID, and perhaps we could establish a convention where a variant keeps the
+//!             KeyID for an exact match at the beginning of the list.
+//!         - An implementation that requires less lookup indirection would be to use the exact keys themselves,
+//!             (or perhaps meaningful keys. See [TableConfig::MEANINGFUL_KEY_LEN]) as the Rocks keys to the CF.
+//!             Then we would separate the exact keys from the "variants" CF, and keys themselves wouldn't be placed
+//!             into the "variants" CF.  Each variant entry would include all of the full keys that it is a variant of.
+//! 
+//!         The decision about which of these implementations is better depends on the average key length.  A 64-bit
+//!         KeyID is the equivalent of an 8-byte key, but an average key is around 16 bytes.  On the other hand,
+//!         eliminating the indirection associated with a second DB query might tip the scales in favor of embedding
+//!         the keys directly.
+//!     
+//!     - The changes above address many records with the same keys, but we may still want to retain the optimization
+//!         where many similar keys are implemented under only one `KeyGroupID` in a variant entry, instead of one
+//!         reference for each individual key.  Especially if those keys are fetched together often.
+//!     
+//!         So, I believe the best path forward looks more like the first option above.  So we would:
+//!         
+//!         - Rework the `KeyGroupID` so it no longer implies a [RecordID] within its lower 44 bits, and is instead
+//!             a completely different number, assigned as needed.
+//! 
+//!         - Rework the key_group entries so that each key is associated with a list of [RecordID]s.
+//! 
+//!         - Rework the "rec_data" CF, so that, instead of a list of key_group indices (which are then converted
+//!             into `KeyGroupID`s), the "rec_data" entry would store the complete list of keys for the record.
+//!
+//!         I believe this approach will improve lookup performance when there are many similar keys belonging to
+//!         different records.  I can identify several downsides however:
+//! 
+//!         - The lookup_exact function no longer has the existing fast path because it must pull the key_group
+//!             entry no matter what, in order to get the [RecordID]s.  This can be partially ameliorated by moving
+//!             the exact match key_group to the beginning of a variant's list.  In addition, the current fast-path
+//!             has a BUG! (see item 8. in this list.)
+//! 
+//!         - The key_group entry will be more expensive to parse on account of it containing a vector of [RecordID]s
+//!             associated with each key.  However, this will be offset by needing to load many fewer key_group
+//!             entries, so I think this may be a net win - or at least a wash.
+//! 
+//!         - The "rec_data" CF will bloat as each key group index (about 1 byte) is replaced by a whole key (avg 16
+//!             bytes).  However, the rec_data is only loaded for bookkeeping operations, and doesn't figure into the
+//!             fuzzy lookup speed.  Therefore the main impact will be DB size, and we can expect a 5-10% increase
+//!             from this factor.
+//! 
+//! 2. BK-Tree search within a key-group.  If we end up with many proximate keys, there may be an advantage to having
+//!     a secondary search structure within the table.  In fact, I think a BL-Tree always outperforms a list, so
+//!     the real question is whether the performance boost justifies the work and code complexity.
+//! 
+//!     SymSpell is very well optimized for sparse key spaces, but in dense key spaces (where many keys share many
+//!     variants), the SymSpell optimization still results in the need to test hundreds of keys with the distance
+//!     function.  In this case, a BK-Tree may speed up the fuzzy lookups substantially.
+//! 
+//! 3. Multi-threading within a lookup. Rework internal plumbing so that variant entries can be fetched and processed
 //!     in parallel, leading to parallel evaluation of key groups and parallel execution of distance functions.
 //! 
-//! 3. Investigate adding a "k-nn lookup" method that would return up to k results, ordered by their distance from the
-//!     lookup key.  Perhaps in addition to, or instead of, the "lookup_best" method.
+//! 4. Investigate adding a "k-nn lookup" method that would return up to k results, ordered by their distance from the
+//!     lookup key.  Perhaps in addition to, or instead of, the [Table::lookup_best] method, because this new method is
+//!     essentially a generalization of [Table::lookup_best]
 //! 
-//! 4. Save the TableConfig to the database, in order to detect an error when the config changes in a way that makes
+//!     This idea is a little bit problematic because a function that returns exactly k results cannot be deterministic
+//!     in cases where there are equal-distance results.  We will often find that k straddles a border and the only
+//!     way to return a deterministic result set is to return more than k results. (or fewer than k, but that may
+//!     mean a set with no results, which is probably not what the caller wants.)
+//! 
+//! 5. Save the TableConfig to the database, in order to detect an error when the config changes in a way that makes
 //!     the database invalid.  Also include a software version check, and create a function to represent which
 //!     software versions contain database format compatibility breaks.  Open Question: Should we store a checksum
 //!     of the distance function?  The function may be re-compiled or changed internally without changing behavior,
 //!     but we can't know that.
 //! 
-//! 5. Remove the `KeyUnsafe` trait as soon as "GenericAssociatedTypes" is stabilized in Rust.  
+//! 6. Remove the `KeyUnsafe` trait as soon as "GenericAssociatedTypes" is stabilized in Rust.  
 //!     <https://github.com/rust-lang/rust/issues/44265>.  As soon as I can implement an associated type with an
 //!     internal lifetime that isn't a parameter to the Key trait itself, then I'll be able to return objects that
 //!     have the same base type but a shorter associated lifetime, and thus eliminate the need to transmute lifetimes.
 //! 
-//! 6. Investigate tools to detect when a [Table] parameter is tuned badly for a data set, based on known optimal
+//! 7. Investigate tools to detect when a [Table] parameter is tuned badly for a data set, based on known optimal
 //!     ratios for certain performance counters.  Alternatively, build tools to assist in performing a parameter
 //!     search optimization process, that could automatically sweep the parameters, searching for the optimal
 //!     [TableConfig] for a given data set.
+//! 
+//! 8. Fix BUG! in [Table::lookup_exact] caused by the optimization where we don't load the key_group associated with
+//!     the exact variant, so we may return records whose keys are supersets of the lookup key.  NOTE: This will
+//!     likely be fixed as a side-effect of the changes proposed in 1.
 //! 
 //! ## Release History
 //! 
@@ -186,25 +273,6 @@
 
 //GOATGOATGOAT, write up:
 // 0.) DNA snippet example.  Look at FAStA Wikipedia article
-// 1.) How SymSpell works best with sparse key spaces, but a BK-Tree is better for dense key spaces
-
-// 2.) How we have "Future Work" to add a BK tree to search within a key group GOAT.  
-//          Redo the "KeyGroups" feature to support keys from different records in the same group.  The current design for KeyGroups optimizes for the case where
-//              a single record has multiple keys with overlapping variants.  This is an efficient design in the case where many proximate keys belong to the same record,
-//              as is the case when multiple spelling variations for the same word are submitted to the Table.
-//              In that case multiple references in a variant entry are replaced with a single KeyGroup reference.
-//              However, the test data makes it clear that different records with similar or identical keys (keys with many overlapping variants) is as big an issue, if not biger.
-//              This should be changed to get full benefit out of any optimizations that improve crowded key-neighborhoods, such as adding a BK-Tree inside of the KeyGroup.
-//          Fuck.  Actually a BK tree in a key-group would do fuck-all without key-groups actually merging keys from multiple records.  That's the real work...  Right now, KeyGroupIDs actually subsume RecordIDs.  For the real perf gain to happen, we'd need separate index spaces.
-
-//GOATGOAT, Document the idea that perhaps there should actually be an exact Keys database where keys or a key hash are the Rocks key.
-//  Changes involved:
-//      Add a "Keys" Rocks table, whose keys are the actual record keys themselves.  Entries here are lists of RecordIDs
-//      Should variants contain key group IDs?  or real Keys?  Real keys are more data, but it means a level of indirection isn't needed.
-//      The answer comes down to what the realistic upper bound is, for the number of keys referenced by a variant.
-//          A 12 character variant with a 26 character alphabet and a delete distance of 2 has roughly 3700 possible keys,
-//          and that number could get nuts for an alphabet linke Chinese.  But the real question is: how many of those
-//          keys exist for a typical variant?
 
 mod unicode_string_helpers;
 mod bincode_helpers;
@@ -682,8 +750,6 @@ mod tests {
         {
             println!("perf_counters feature not enabled");
         }
-
-
     }
 
 }
